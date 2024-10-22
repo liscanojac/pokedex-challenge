ede59832b2aa1760c8e320937007b260ba524416
Juan Canelon <liscanojac@gmail.com>
2024-10-22 15:15:56 -0300

    feat: setting ui colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'Sans-serif'],
+      },
+      colors: {
+        'primary-yellow': '#eca539',
+        'dark-red': '#c00e20',
+        'primary-red': '#f22539',
+        'primary-black': '#353535',
+        'dark-grey': '#5e5e5e',
+        'primary-grey': '#bfbfbf',
+        'secondary-grey': '#e8e8e8',
+        'primary-white': '#f5f5f5',
+        'secondary-white': '#f9f9f9',
+      },
+    },
   },
   plugins: [],
 }
