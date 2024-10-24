9c1cb89781fb86c12b93501de400619cc1174961
Juan Canelon <liscanojac@gmail.com>
2024-10-24 18:02:48 -0300

    feat: loading-spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div :class="['spinner', spinnerClasses]">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'YourComponent',
+  props: {
+    spinnerClasses: {
+      type: String,
+    },
+  },
+})
+</script>
+
+<style scoped>
+.spinner {
+  animation: spin 1s linear infinite;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
