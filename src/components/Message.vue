40ae7a20009835c38d1e5b77c518449ccc8e16b9
Juan Canelon <liscanojac@gmail.com>
2024-10-24 18:01:31 -0300

    feat: message component

---
+++ src/components/Message.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col items-center justify-center text-center">
+    <h1 :class="['text-4xl font-bold my-2', titleClasses]">{{ title }}</h1>
+    <h3 :class="['text-xl my-2', msgClasses]">{{ msg }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MessageComponent',
+  props: {
+    title: {
+      type: String,
+    },
+    msg: {
+      type: String,
+    },
+    titleClasses: {
+      type: String,
+    },
+    msgClasses: {
+      type: String,
+    },
+  },
+})
+</script>
