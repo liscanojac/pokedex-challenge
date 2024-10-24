29a50fc746ffa1e0f3102a039db2f4e49acb0d07
Juan Canelon <liscanojac@gmail.com>
2024-10-24 17:56:03 -0300

    feat: intersectionObserver

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,15 @@
 <template>
-  <div v-for="pokemon in pokemonsToShow" :key="pokemon.id" @scroll="testFunc">
-    <Card :pokemon="pokemon"></Card>
+  <div class="my-8">
+    <div v-for="pokemon in pokemonsToShow" :key="pokemon.id">
+      <Card :pokemon="pokemon"></Card>
+    </div>
+    <Spinner
+      v-if="isLoading && pokemonsToShow.length && pokemonSelection === 'all'"
+      spinner-classes="w-10 mb-28 mt-5"
+    >
+      <img src="../assets/Loader.png" alt="loading-spinner" />
+    </Spinner>
+    <div ref="target"></div>
   </div>
 </template>
 
@@ -11,26 +20,51 @@ import { defineComponent } from 'vue'
 import Card from './Card.vue'
 import { usePagesStore } from '@/stores/pages'
 import type { PokemonBase } from '@/interfaces/pokemon'
+import { useIntersectionObserver } from '@vueuse/core'
+import Spinner from './Spinner.vue'
 
 export default defineComponent({
   name: 'ListComponent',
   props: {},
   components: {
     Card,
+    Spinner,
   },
   async created() {
     this.startLoading()
-    await this.getPokemon()
+    if (!this.apiCallSuccess) await this.showAllPokemon()
+    this.resetAPiCallSuccess()
     this.stopLoading()
+  },
+  mounted() {
+    const target = this.$refs.target as HTMLElement
+    useIntersectionObserver(target, async ([{ isIntersecting }]) => {
+      if (
+        isIntersecting &&
+        this.pokemonSelection === 'all' &&
+        this.getCurrentPage > 0
+      ) {
+        console.log(this.getCurrentPage)
+        this.startLoading()
+        if (!this.apiCallSuccess) await this.getPokemon()
+        this.resetAPiCallSuccess()
+        this.stopLoading()
+      }
+    })
   },
   computed: {
     ...mapState(usePokemonStore, [
       'pokemons',
       'getFavoritePokemons',
-      'favoriteSelected',
+      'pokemonSelection',
+      'apiCallSuccess',
+      'isLoading',
     ]),
+    ...mapState(usePagesStore, ['getCurrentPage']),
     pokemonsToShow(): Array<PokemonBase> {
-      return this.favoriteSelected ? this.getFavoritePokemons : this.pokemons
+      return this.pokemonSelection === 'favorite'
+        ? this.getFavoritePokemons
+        : this.pokemons
     },
   },
   methods: {
@@ -38,15 +72,10 @@ export default defineComponent({
       'getPokemon',
       'startLoading',
       'stopLoading',
+      'showAllPokemon',
+      'resetAPiCallSuccess',
     ]),
     ...mapActions(usePagesStore, ['nextPage']),
-    testFunc() {
-      console.log('marico')
-    },
   },
 })
 </script>
-
-<style scoped>
-/* Add your styles here */
-</style>
